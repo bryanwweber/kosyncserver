5a2da92f7cb89a29b8638b370eef52bd5c61480f
Bryan Weber <bryan.w.weber@gmail.com>
2025-09-03 09:53:06 -0400

    Switch to structlog instead of loguru
    This has a few tutorials online to integrate with FastAPI and seems to
    work pretty well.

--- src/kosyncserver/documents.py
+++ src/kosyncserver/documents.py
@@ -2,15 +2,17 @@ import time
 from typing import Annotated
 
 import aiosqlite
+import structlog
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
-from loguru import logger
 from pydantic import BaseModel
 
 from .database import get_db
+from .logging import Logger
 from .users import get_user
 
 router = APIRouter()
+logger: Logger = structlog.get_logger()
 
 
 class RequestPosition(BaseModel):
@@ -33,37 +35,29 @@ class ResponsePosition(BaseModel):
     timestamp: int
 
 
-select_query = """
-SELECT *
-FROM documents
-WHERE document_id = :document_id
-ORDER BY timestamp DESC
-LIMIT 1
-"""
-
-
 @router.get("/syncs/progress/{document_id}", response_model=RequestPosition)
 async def get_sync_progress(
     document_id: str,
     db: Annotated[aiosqlite.Connection, Depends(get_db)],
     _: Annotated[None, Depends(get_user)],
 ):
-    logger.debug("Getting sync progress for {document_id}", document_id=document_id)
+    select_query = """
+    SELECT *
+    FROM documents
+    WHERE document_id = :document_id
+    ORDER BY timestamp DESC
+    LIMIT 1
+    """
+
+    logger.debug("Getting sync progress for document", document_id=document_id)
     db.row_factory = document_to_request_position
     result = await db.execute(select_query, {"document_id": document_id})
     document = await result.fetchone()
-    if document:
-        return document
-    else:
+    if not document:
+        logger.debug("Document not found")
         return JSONResponse(content={"message": "Document not found!"}, status_code=404)
-
-
-update_query = """
-INSERT INTO documents (document_id, percentage, progress, device, device_id, timestamp)
-VALUES (:document_id, :percentage, :progress, :device, :device_id, :timestamp)
-ON CONFLICT (document_id, device_id)
-DO UPDATE SET percentage = :percentage, progress = :progress, timestamp = :timestamp
-"""
+    logger.debug("Document found", **document.model_dump())
+    return document
 
 
 @router.put("/syncs/progress", response_model=ResponsePosition)
@@ -73,6 +67,15 @@ async def update_sync_progress(
     _: Annotated[None, Depends(get_user)],
 ):
     now = int(time.time())
+    update_query = """
+    INSERT INTO documents (
+        document_id, percentage, progress, device, device_id, timestamp
+    )
+    VALUES (:document_id, :percentage, :progress, :device, :device_id, :timestamp)
+    ON CONFLICT (document_id, device_id)
+    DO UPDATE SET percentage = :percentage, progress = :progress, timestamp = :timestamp
+    """
+
     await db.execute(
         update_query,
         {
@@ -85,6 +88,7 @@ async def update_sync_progress(
         },
     )
     await db.commit()
+    logger.debug("Sync progress updated for document", **request.model_dump())
     return ResponsePosition(
         document_id=request.document_id,
         timestamp=now,
